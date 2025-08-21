4bc22a4ceee8efe9c1bb64a15e36e02d41d7aa9f
GAMGO <gamgo445@gmail.com>
2025-08-21 17:15:30 +0900

    commentAPI Added

---
+++ spring_6board/src/main/java/org/iclass/board/controller/APICommentsController.java
@@ -0,0 +1,14 @@
+package org.iclass.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@RestController
+public class APICommentsController {
+@GetMapping("/api/commnts")
+public int select(){
+    
+}
+}
